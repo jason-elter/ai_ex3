8ec468e0494df88193f0175a1a4ac59f28353f21
EranHashmo <eranhashmo@gmail.com>
2021-05-28 00:30:58 +0300

    think I finished

--- hanoi.py
+++ hanoi.py
@@ -6,7 +6,7 @@ def create_domain_file(domain_file_name, n_, m_):
     pegs = ['p_%s' % i for i in list(range(m_))]  # [p_0,..., p_(m_ - 1)]
     domain_file = open(domain_file_name, 'w')  # use domain_file.write(str) to write to domain_file
     "*** YOUR CODE HERE ***"
-    domain_file.write("Propositions:")
+    domain_file.write("Propositions:\n")
     # propositions regarding what spots are available (have nothing above them).
     for peg in pegs:
         domain_file.write(" available-" + peg)
@@ -31,6 +31,8 @@ def create_domain_file(domain_file_name, n_, m_):
         for peg in pegs:
             domain_file.write(" smaller-" + disk1 + "-" + peg)
 
+    domain_file.write("\nActions:")
+    # all possible moves
     for disk in disks:
         for from_item in disks + pegs:
             if disk == from_item:
@@ -38,24 +40,23 @@ def create_domain_file(domain_file_name, n_, m_):
             for to_item in disks + pegs:
                 if disk == to_item or from_item == to_item:
                     continue
-                domain_file.write("\nOperator move-" +
+                domain_file.write("\nName: move-" +
                                   disk + "-" +
                                   from_item + "-" +
                                   to_item)
 
-                domain_file.write("\nPRE: on-" + disk + "-" + from_item + "," +
-                                  " available-" + disk + "," +
-                                  " available-" + to_item + "," +
+                domain_file.write("\npre: on-" + disk + "-" + from_item +
+                                  " available-" + disk +
+                                  " available-" + to_item +
                                   " smaller-" + disk + "-" + to_item)
 
-                domain_file.write(" ADD: on-" + disk + "-" + to_item + "," +
+                domain_file.write("\nadd: on-" + disk + "-" + to_item +
                                   " available-" + from_item)
 
-                domain_file.write(" DEL: on-" + disk + "-" + from_item + "," +
+                domain_file.write("\ndel: on-" + disk + "-" + from_item +
                                   " available-" + to_item)
 
     domain_file.close()
-
 
 
 def create_problem_file(problem_file_name_, n_, m_):
